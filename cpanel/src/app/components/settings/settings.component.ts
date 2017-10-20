9827a04e0a1bdceecbaad1961e28c0b4cc39eb9c
OstapHetman <prickly4528@gmail.com>
2017-10-20 16:28:41 +0300

    Adding The Settings Page

--- cpanel/src/app/components/settings/settings.component.ts
+++ cpanel/src/app/components/settings/settings.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
+import { Router} from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Settings } from '../../models/Settings';
+
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private flashMessagesService: FlashMessagesService,
+    public settingsService: SettingsService
+  ) { }
 
   ngOnInit() {
+    this.settings = this.settingsService.getSettings();
   }
-
+  onsubmit() {
+    this.settingsService.changeSettings(this.settings);
+    this.flashMessagesService.show('Settings saved', {cssClass: 'alert-success', timeout: 4000});
+    this.router.navigate(['/settings']);
+  }
 }
